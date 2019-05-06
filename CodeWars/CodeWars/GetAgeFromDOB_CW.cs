c429bbc4082ed8fb2095547f818f1c022d0d28ac
Artem G <ofdm@i.ua>
2019-05-06 23:17:36 +0300

    GetAgeFromDOB updated with Codewars ver

---
+++ CodeWars/CodeWars/GetAgeFromDOB_CW.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json;
+
+namespace CodeWars
+{
+    public sealed class Katas
+    {
+        private readonly IClocks clock;
+
+        public Katas(IClocks clock)
+        {
+            this.clock = clock;
+        }
+
+        public int GetAgeFromDOB_CW(DateTime dob)
+        {
+            var now = this.clock.Now;
+
+            return now.Year - dob.Year - (
+              now.Month < dob.Month || now.Month == dob.Month && now.Day < dob.Day ? 1 : 0);
+        }
+    }
+
+    public interface IClocks
+    {
+        DateTime Now { get; }
+    }
+
+    public sealed class SystemClocks : IClock
+    {
+        public DateTime Now => DateTime.Now;
+    }
+
+    public sealed class StaticClocks : IClock
+    {
+        private readonly DateTime value;
+
+        public StaticClocks(DateTime value)
+        {
+            this.value = value;
+        }
+
+        public DateTime Now => this.value;
+    }
+
+}
+
--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -11,11 +11,14 @@ namespace CodeWars
     {
         static void Main()
         {
+            #region GetAgeFromDOB
+            //  GetAgeFromDOB
+
             Kata kata = new Kata(new StaticClock(new DateTime(1989, 03, 1)));
             int i = kata.GetAgeFromDOB(new DateTime(1988, 02, 29));
-
             Console.WriteLine($"Age: {i}  stat.Now: {kata.stat.Now}  syst.Now: {kata.syst.Now}");
             Console.ReadKey();
+            #endregion
         }
     }
 }
