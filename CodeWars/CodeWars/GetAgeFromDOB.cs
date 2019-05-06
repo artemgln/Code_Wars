593d0072e717849c069738ed0dd9e25106246c41
Artem G <ofdm@i.ua>
2019-05-06 23:03:52 +0300

    1st commit

---
+++ CodeWars/CodeWars/GetAgeFromDOB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json;
+
+namespace CodeWars
+{
+    public class Kata : IClock
+    {
+        public StaticClock stat;
+        public SystemClock syst = new SystemClock();
+
+        public Kata(StaticClock smth)
+        {
+            stat = smth;
+        }
+
+        public DateTime Now { get; set; }
+
+        public int GetAgeFromDOB(DateTime birthday)
+        {
+            int years = stat.Now.Year - birthday.Year;
+            int months = stat.Now.Month - birthday.Month;
+            int days = stat.Now.Day - birthday.Day;
+
+            if (months < 0) { years--; }
+            if (months == 0 & days < 0) { years--; }
+            // Returns age in years
+            return years;
+        }
+    }
+
+    public interface IClock
+    {
+        DateTime Now { get; }
+    }
+
+    public class SystemClock : IClock
+    {
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+    }
+
+    public class StaticClock : IClock
+    {
+        public DateTime Now { get; }
+
+        public StaticClock(DateTime input)
+        {
+            Now = input;
+        }
+    }
+
+
+}
+
---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main()
+        {
+            Kata kata = new Kata(new StaticClock(new DateTime(1989, 03, 1)));
+            int i = kata.GetAgeFromDOB(new DateTime(1988, 02, 29));
+
+            Console.WriteLine($"Age: {i}  stat.Now: {kata.stat.Now}  syst.Now: {kata.syst.Now}");
+            Console.ReadKey();
+        }
+    }
+}
