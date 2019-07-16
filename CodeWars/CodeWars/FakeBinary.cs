245d01ffbdf3ceb1335f6b3d1219e806a0d30e72
Artem G <ofdm@i.ua>
2019-07-17 00:09:55 +0200

    added Fake Binary with expressions

---
+++ CodeWars/CodeWars/FakeBinary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class FakeBinary
+    {
+        public static string FakeBin(string x)
+        {
+            // <5 return - 0, >5 - 1
+            string ans = String.Empty;
+            foreach (char c in x)
+            {
+                int z = ("0123456789".IndexOf(c));
+                if (z>4) { ans+="1"; }
+                else { ans+="0"; }
+            }
+            return ans;
+        }
+
+        public static string FakeBin_Regex(string x)
+        {
+            x = Regex.Replace(x, "[4321]", "0");
+            x = Regex.Replace(x, "[56789]", "1");
+            return x;
+        }
+    }
+}
--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -11,13 +11,26 @@ namespace CodeWars
     {
         static void Main()
         {
+
+            #region FakeBinary
+            //  FakeBinary
+            string i = FakeBinary.FakeBin("27081984");
+
+            Console.WriteLine("Hello");
+            Console.WriteLine($"Converted number: {i} ");
+            Console.ReadKey();
+            //
+            #endregion
+
+
             #region GetAgeFromDOB
-            //  GetAgeFromDOB
+            /*/  GetAgeFromDOB
 
             Kata kata = new Kata(new StaticClock(new DateTime(1989, 03, 1)));
             int i = kata.GetAgeFromDOB(new DateTime(1988, 02, 29));
             Console.WriteLine($"Age: {i}  stat.Now: {kata.stat.Now}  syst.Now: {kata.syst.Now}");
             Console.ReadKey();
+            */
             #endregion
         }
     }
