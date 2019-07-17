d193f0a1b6b89faf28b615aab20bbaa889669ea1
Artem G <ofdm@i.ua>
2019-07-18 00:17:46 +0200

    added csv convert

--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -11,17 +11,24 @@ namespace CodeWars
     {
         static void Main()
         {
+            #region SortCSV
+            string v = @"a;e;c;b;d\n
+            3907; 145; 191; 18; 12\n
+            48; 2; 12; 13; 11";
+
+            Console.WriteLine($"Converted number: {SortCsvColumns.SortCsvCol(v)}");
+            Console.ReadKey();
+            #endregion
 
             #region FakeBinary
-            //  FakeBinary
+            /*/  FakeBinary
             string i = FakeBinary.FakeBin("27081984");
 
             Console.WriteLine("Hello");
             Console.WriteLine($"Converted number: {i} ");
             Console.ReadKey();
-            //
+            /*/
             #endregion
-
 
             #region GetAgeFromDOB
             /*/  GetAgeFromDOB
@@ -30,13 +37,15 @@ namespace CodeWars
             int i = kata.GetAgeFromDOB(new DateTime(1988, 02, 29));
             Console.WriteLine($"Age: {i}  stat.Now: {kata.stat.Now}  syst.Now: {kata.syst.Now}");
             Console.ReadKey();
-            */
+            /*/
             #endregion
 
             #region ReplaceWithAlphabetPosition
+            /*/
             string ab=ReplaceWithAlphabetPosition3.AlphabetPosition("Abc Xyz");
             Console.WriteLine($"ReplaceWithAlphabetPosition2 result: {ab}");
             Console.ReadKey();
+            /*/
             #endregion
         }
     }
---
+++ CodeWars/CodeWars/SortCsvColumns.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class SortCsvColumns
+    {
+        public static string SortCsvCol(string csvFileContent)
+        {
+            StringReader strReader = new StringReader(csvFileContent);
+            string line;
+            Dictionary<int, string[]> csv = new Dictionary<int, string[]>();
+            int i = 0;
+            while ((line = strReader.ReadLine()) != null)
+            {
+                csv.Add(i, line.Split(';'));
+                i++;
+            }
+            var array=csv.ToArray();
+            return csvFileContent;
+        }
+    }
+}
